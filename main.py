b92fd1a0b008f534d32d76e382f8a648f5eb7ef5
Sepehr Pashoutani <sepp@sepehrs-mbp.unomaha.edu>
2022-02-07 13:19:34 -0600

    Update the timezone info

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import config
 import alpaca_trade_api as tradeapi
 from alpaca_trade_api.stream import Stream
 import matplotlib.pyplot as plt
-from alpaca_trade_api.rest import REST
+from alpaca_trade_api.rest import REST, TimeFrame
 import pandas as pd
 from datetime import date
 import time
@@ -13,7 +13,7 @@ import time
 # Assign constant variables and use in the rest of the script.
 ##############################################################
 ticker = 'AMC'
-condition = 1
+condition = 0
 start_day = date.today()
 api_key = config.api_key
 api_secret = config.api_secret
@@ -32,23 +32,29 @@ try:
     print(f'Cash available: {account.cash}', end='\n')
     print(f'Buying power: {account.buying_power}', end='\n')
 
-    time.sleep(5)
+    time.sleep(1)
 
 except:
 
     print('Account authentication failed')
-    time.sleep(5)
+    time.sleep(1)
 ##############################################################
 ##############################################################
 
 while condition == 0:
     
-    data = REST.get_bars(symbol = ticker,
-                        timeframe = tradeapi.TimeFrameUnit.Minute,
-                        adjustment='raw')
+    data = api.get_bars(symbol = ticker,
+                        timeframe = '1Min',
+                        adjustment='raw').df
+    print(data.shape)
+    data = data.resample('1Min').mean().bfill()
+    print(data.shape)
+    data.index = data.index.tz_convert('US/Central')
 
 
     condition = 1
 
+print(data.head())
+print('\n')
 print(data.tail())
 
